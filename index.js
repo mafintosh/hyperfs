8fcf8b9198fcdb39f87365a5b2d65f529d2f2969
Mathias Buus <mathiasbuus@gmail.com>
2015-05-21 14:21:46 +0200

    add statfs

--- index.js
+++ index.js
@@ -470,6 +470,22 @@ module.exports = function (home) {
       ops.force = true
       ops.options = ['suid', 'dev']
 
+      ops.statfs = function (pathname, cb) { // TODO: return actual corrent data here instead
+        cb(0, {
+          bsize: 1000000,
+          frsize: 1000000,
+          blocks: 1000000,
+          bfree: 1000000,
+          bavail: 1000000,
+          files: 1000000,
+          ffree: 1000000,
+          favail: 1000000,
+          fsid: 1000000,
+          flag: 1000000,
+          namemax: 1000000
+        })
+      }
+
       ops.link = function (name, dest, cb) {
         cow(name, function (err, file) {
           if (err) return cb(fuse.errno(err.code))
